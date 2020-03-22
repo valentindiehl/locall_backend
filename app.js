d90a9a5f1b3f69f4c03f38385f253b277e83e333
Jonas Müller <jonas.mueller@compitencies.com>
2020-03-22 03:04:19 +0100

    Import socket.io boilerplate code

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ mongoose.promise = global.Promise;
 
 const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
+var server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(cors());
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'sec', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+app.use(session({secret: 'sec', cookie: {maxAge: 60000}, resave: false, saveUninitialized: false}));
 
-if(!isProduction) {
-  app.use(errorHandler());
+if (!isProduction) {
+	app.use(errorHandler());
 }
 
 mongoose.connect('mongodb://locallmasterblaster:tivxos-vYpfo3-nehreq@docdb-2020-03-21-23-23-08.cluster-chjfvfcgw69a.eu-central-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false');
@@ -28,28 +30,39 @@ require('./models/Users');
 require('./config/passport');
 app.use(require('./routes'));
 
-if(!isProduction) {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
+if (!isProduction) {
+	app.use((err, req, res) => {
+		res.status(err.status || 500);
 
-    res.json({
-      errors: {
-        message: err.message,
-        error: err,
-      },
-    });
-  });
+		res.json({
+			errors: {
+				message: err.message,
+				error: err,
+			},
+		});
+	});
 }
 
 app.use((err, req, res) => {
-  res.status(err.status || 500);
+	res.status(err.status || 500);
 
-  res.json({
-    errors: {
-      message: err.message,
-      error: {},
-    },
-  });
+	res.json({
+		errors: {
+			message: err.message,
+			error: {},
+		},
+	});
 });
 
-app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
+// IO Events
+
+io.on('connection', function (socket) {
+	console.log("New Connection!");
+	socket.on('message', function () {
+		// TODO
+	});
+});
+
+server.listen(8000, () => console.log('Server running on http://localhost:8000/'));
+
+
