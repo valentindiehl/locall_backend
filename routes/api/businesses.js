3279894f46ae49566df028df0bacc63464db9274
christopher@familie-voit.de <christopher@familie-voit.de>
2020-03-29 00:33:52 +0100

    Added LoginError Logik

--- routes/api/businesses.js
+++ routes/api/businesses.js
@@ -4,13 +4,10 @@ const mongoose = require('mongoose');
 const Businesses = mongoose.model('Businesses');
 
 router.post('/', auth.optional, (req, res, next) => {
-   const { body: { business } } = req;
-   console.log(req.body);
-
-   const finalBusiness = new Businesses(business);
-
-   return finalBusiness.save()
-       .then(() => res.json({ business: finalBusiness.toJSON() }));
+    const {body: {business}} = req;
+    const finalBusiness = new Businesses(business);
+    return finalBusiness.save()
+        .then(() => res.json({business: finalBusiness.toJSON()}));
 });
 
 router.get('/', auth.optional, (req, res, next) => {
@@ -18,7 +15,7 @@ router.get('/', auth.optional, (req, res, next) => {
         let businessList = {};
         businessList["data"] = [];
         console.log(businesses);
-        businesses.forEach(function(business) {
+        businesses.forEach(function (business) {
             businessList["data"].push(business);
         });
 
@@ -28,16 +25,14 @@ router.get('/', auth.optional, (req, res, next) => {
 
 router.get('/:id', auth.optional, (req, res, next) => {
     Businesses.findOne({id: req.params.id}, (err, business) => {
-        if (err)
-        {
+        if (err) {
             console.log(err);
-            return res.status(500).json({ 'error': 'Internal server error. Please try again later.'});
+            return res.status(500).json({'error': 'Internal server error. Please try again later.'});
         }
-        if (business != null)
-        {
+        if (business != null) {
             return res.json(business);
         }
-        return res.status(404).json({ 'error': 'Could not find business with this ID.'});
+        return res.status(404).json({'error': 'Could not find business with this ID.'});
     });
 });
 
