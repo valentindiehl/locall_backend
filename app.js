f24da8ec99539f160e42bae7d6ab8a2d71ac4be3
Valentin Diehl <valentin.diehl@tum.de>
2020-04-09 11:39:23 +0200

    Added Profile capacities

--- app.js
+++ app.js
@@ -64,28 +64,31 @@ require('./config/passport');
 app.use("/", require('./routes'));
 
 if (!isProduction) {
-	app.use((err, req, res) => {
+	app.use((err, req, res, next) => {
 		res.status(err.status || 500);
 
 		res.json({
-			errors: {
-				message: err.message,
+			error: {
+				message: err.code === "LIMIT_FILE_SIZE" ? "Bitte lade nur Dateien unter 2MB hoch." : err.message,
 				error: err,
 			},
 		});
 	});
 }
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
 	res.status(err.status || 500);
 
 	res.json({
-		errors: {
+		error: {
 			message: err.message,
 			error: {},
 		},
 	});
 });
+
+app.use(express.static('public'));
+
 
 // socket.io handling
 // Documentation to our socket.io events here:
